ddac24a54a10669d6fc978196d753d6dc2ab2331
bmjd <bmjd@foxmail.com>
2016-05-11 18:54:44 +0800

    test5

--- Chapter 6/Kamera_Final/Kamera/THVideoPlayer/Controllers/THPlayerController.m
+++ Chapter 6/Kamera_Final/Kamera/THVideoPlayer/Controllers/THPlayerController.m
@@ -67,48 +67,30 @@ static const NSString *PlayerItemStatusContext;
 
 #pragma mark - Setup
 
-//- (id)initWithURL:(NSURL *)assetURL {
-//    self = [super init];
-//    if (self) {
-//        _asset = [AVAsset assetWithURL:assetURL];                           // 1
-//        [self prepareToPlay];
-//    }
-//    return self;
-//}
 - (id)initWithURLs:(NSArray *)assetURLs;
 {
     self = [super init];
     if (self) {
         _assetURLs = [assetURLs mutableCopy];
-//        _asset = [AVAsset assetWithURL:self.assetURLs.firstObject];
         [self prepareToPlay];
     }
     return self;
 }
 - (void)prepareToPlay {
-    NSArray *keys = @[
-        @"tracks",
-        @"duration",
-        @"commonMetadata",
-        @"availableMediaCharacteristicsWithMediaSelectionOptions"
-    ];
-
+    
     self.playerItems = @[].mutableCopy;
     CMTime duration = kCMTimeZero;
-
+    
     for (NSURL *aURL in self.assetURLs) {
-        AVAsset *asset = [AVAsset assetWithURL:aURL];
-        AVPlayerItem *playerItem = [AVPlayerItem playerItemWithAsset:asset automaticallyLoadedAssetKeys:keys];
-//        AVPlayerItem *playerItem = [AVPlayerItem playerItemWithAsset:asset];
         NSDictionary *opts = [NSDictionary dictionaryWithObject:[NSNumber numberWithBool:NO]
                                                          forKey:AVURLAssetPreferPreciseDurationAndTimingKey];
         AVURLAsset *urlAsset = [AVURLAsset URLAssetWithURL:aURL options:opts];  // 初始化视频媒体文件
         CMTimeShow(urlAsset.duration);
-        playerItem = [AVPlayerItem playerItemWithAsset:urlAsset];
+        AVPlayerItem *playerItem = [AVPlayerItem playerItemWithAsset:urlAsset];
         [playerItem addObserver:self                                       // 3
-                          forKeyPath:STATUS_KEYPATH
-                             options:0
-                             context:&PlayerItemStatusContext];
+                     forKeyPath:STATUS_KEYPATH
+                        options:0
+                        context:&PlayerItemStatusContext];
         [self.playerItems addObject:playerItem];
         CMTime tmp = playerItem.duration;
         duration = CMTimeAdd(tmp, duration);
@@ -117,7 +99,7 @@ static const NSString *PlayerItemStatusContext;
     self.player = [AVQueuePlayer queuePlayerWithItems:self.playerItems];
     self.playerItem = self.playerItems.firstObject;
     self.playerItemIndex = 0;
-
+    
     self.playerView = [[THPlayerView alloc] initWithPlayer:self.player];    // 5
     self.transport = self.playerView.transport;
     self.transport.delegate = self;
@@ -132,28 +114,17 @@ static const NSString *PlayerItemStatusContext;
         
         dispatch_async(dispatch_get_main_queue(), ^{                        // 1
             
-//            [self.playerItem removeObserver:self forKeyPath:STATUS_KEYPATH];
-            
+            //            [self.playerItem removeObserver:self forKeyPath:STATUS_KEYPATH];
             if (self.playerItem.status == AVPlayerItemStatusReadyToPlay) {
-                
                 // Set up time observers.                                   // 2
                 [self addPlayerItemTimeObserver];
                 [self addItemEndObserverForPlayerItem];
-                
-
-                
                 // Synchronize the time display                             // 3
                 [self.transport setCurrentTime:CMTimeGetSeconds(kCMTimeZero)
                                       duration:self.queueDuration];
-                
                 // Set the video title.
                 [self.transport setTitle:[self.playerItems[_playerItemIndex] asset].title];                 // 4
-                
                 [self.player play];                                         // 5
-                
-//                [self loadMediaOptions];
-//                [self generateThumbnails];
-                
             } else {
                 [UIAlertView showAlertWithTitle:@"Error"
                                         message:@"Failed to load video"];
@@ -162,39 +133,6 @@ static const NSString *PlayerItemStatusContext;
     }
 }
 
-//- (void)loadMediaOptions {
-//    NSString *mc = AVMediaCharacteristicLegible;                            // 1
-//    AVMediaSelectionGroup *group =
-//        [self.asset mediaSelectionGroupForMediaCharacteristic:mc];          // 2
-//    if (group) {
-//        NSMutableArray *subtitles = [NSMutableArray array];                 // 3
-//        for (AVMediaSelectionOption *option in group.options) {
-//            [subtitles addObject:option.displayName];
-//        }
-//        [self.transport setSubtitles:subtitles];                            // 4
-//    } else {
-//        [self.transport setSubtitles:nil];
-//    }
-//}
-//
-//- (void)subtitleSelected:(NSString *)subtitle {
-//    NSString *mc = AVMediaCharacteristicLegible;
-//    AVMediaSelectionGroup *group =
-//        [self.asset mediaSelectionGroupForMediaCharacteristic:mc];          // 1
-//    BOOL selected = NO;
-//    for (AVMediaSelectionOption *option in group.options) {
-//        if ([option.displayName isEqualToString:subtitle]) {
-//            [self.playerItem selectMediaOption:option                       // 2
-//                         inMediaSelectionGroup:group];
-//            selected = YES;
-//        }
-//    }
-//    if (!selected) {
-//        [self.playerItem selectMediaOption:nil                              // 3
-//                     inMediaSelectionGroup:group];
-//    }
-//}
-
 
 #pragma mark - Time Observers
 
@@ -202,45 +140,44 @@ static const NSString *PlayerItemStatusContext;
     
     // Create 0.5 second refresh interval - REFRESH_INTERVAL == 0.5
     CMTime interval =
-        CMTimeMakeWithSeconds(REFRESH_INTERVAL, NSEC_PER_SEC);              // 1
+    CMTimeMakeWithSeconds(REFRESH_INTERVAL, NSEC_PER_SEC);              // 1
     
     // Main dispatch queue
     dispatch_queue_t queue = dispatch_get_main_queue();                     // 2
     
     // Create callback block for time observer
     __weak THPlayerController *weakSelf = self;                             // 3
-    void (^callback)(CMTime time) = ^(CMTime time) {
-        NSTimeInterval currentTime = CMTimeGetSeconds(time);
-        NSTimeInterval duration = CMTimeGetSeconds(weakSelf.playerItem.duration);
-        [weakSelf.transport setCurrentTime:currentTime duration:duration];  // 4
-    };
     
     // Add observer and store pointer for future use
     self.timeObserver =                                                     // 5
-        [self.player addPeriodicTimeObserverForInterval:interval
-                                                  queue:queue
-                                             usingBlock:callback];
+    [self.player addPeriodicTimeObserverForInterval:interval
+                                              queue:queue
+                                         usingBlock:^(CMTime time) {
+                                             NSTimeInterval currentTime = CMTimeGetSeconds(time);
+                                             NSTimeInterval duration = CMTimeGetSeconds(weakSelf.playerItem.duration);
+                                             [weakSelf.transport setCurrentTime:currentTime duration:duration];  // 4
+                                         }];
 }
 
 - (void)addItemEndObserverForPlayerItem {
-
+    
     NSString *name = AVPlayerItemDidPlayToEndTimeNotification;
-
+    
     NSOperationQueue *queue = [NSOperationQueue mainQueue];
-
+    
     __weak THPlayerController *weakSelf = self;                             // 1
     void (^callback)(NSNotification *note) = ^(NSNotification *notification) {
         [weakSelf.player seekToTime:kCMTimeZero                             // 2
                   completionHandler:^(BOOL finished) {
-            [weakSelf.transport playbackComplete];                          // 3
-        }];
+                      [weakSelf.transport playbackComplete];                          // 3
+                  }];
     };
-
+    
     self.itemEndObserver =                                                  // 4
-        [[NSNotificationCenter defaultCenter] addObserverForName:name
-                                                          object:self.playerItem
-                                                           queue:queue
-                                                      usingBlock:callback];
+    [[NSNotificationCenter defaultCenter] addObserverForName:name
+                                                      object:self.playerItem
+                                                       queue:queue
+                                                  usingBlock:callback];
 }
 
 #pragma mark - THTransportDelegate Methods
@@ -280,63 +217,6 @@ static const NSString *PlayerItemStatusContext;
         [self.player play];
     }
 }
-
-/*
-#pragma mark - Thumbnail Generation
-
-- (void)generateThumbnails {
-    
-    self.imageGenerator =                                                   // 1
-        [AVAssetImageGenerator assetImageGeneratorWithAsset:self.asset];
-    
-    // Generate the @2x equivalent
-    self.imageGenerator.maximumSize = CGSizeMake(200.0f, 0.0f);             // 2
-
-    CMTime duration = self.asset.duration;
-
-    NSMutableArray *times = [NSMutableArray array];                         // 3
-    CMTimeValue increment = duration.value / 20;
-    CMTimeValue currentValue = 2.0 * duration.timescale;
-    while (currentValue <= duration.value) {
-        CMTime time = CMTimeMake(currentValue, duration.timescale);
-        [times addObject:[NSValue valueWithCMTime:time]];
-        currentValue += increment;
-    }
-
-    __block NSUInteger imageCount = times.count;                            // 4
-    __block NSMutableArray *images = [NSMutableArray array];
-
-    AVAssetImageGeneratorCompletionHandler handler;                         // 5
-    
-    handler = ^(CMTime requestedTime,
-                CGImageRef imageRef,
-                CMTime actualTime,
-                AVAssetImageGeneratorResult result,
-                NSError *error) {
-
-        if (result == AVAssetImageGeneratorSucceeded) {                     // 6
-            UIImage *image = [UIImage imageWithCGImage:imageRef];
-            id thumbnail =
-                [THThumbnail thumbnailWithImage:image time:actualTime];
-            [images addObject:thumbnail];
-        } else {
-            NSLog(@"Error: %@", [error localizedDescription]);
-        }
-
-        // If the decremented image count is at 0, we're all done.
-        if (--imageCount == 0) {                                            // 7
-            dispatch_async(dispatch_get_main_queue(), ^{
-                NSString *name = THThumbnailsGeneratedNotification;
-                NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
-                [nc postNotificationName:name object:images];
-            });
-        }
-    };
-
-    [self.imageGenerator generateCGImagesAsynchronouslyForTimes:times       // 8
-                                              completionHandler:handler];
-}
-*/
 
 #pragma mark - Housekeeping
 
